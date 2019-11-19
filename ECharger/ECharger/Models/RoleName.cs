a0d899e8e66aa9cf3871ac873416c9e30994cd33
Ricardo <33632491+rmcsilva@users.noreply.github.com>
2019-11-19 13:23:57 +0000

    Added: Role names
    Constants to represent each role

---
+++ ECharger/ECharger/Models/RoleName.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ECharger.Models
+{
+    public static class RoleName
+    {
+        public const string Admin = "admin";
+        public const string Company = "company";
+        public const string User = "user";
+    }
+}
