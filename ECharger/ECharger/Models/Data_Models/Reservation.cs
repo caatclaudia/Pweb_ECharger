bf2bce4d5628b86d5fd191a9a516f405ebe32067
Ricardo <33632491+rmcsilva@users.noreply.github.com>
2019-11-20 12:30:20 +0000

    Updated: Merged Reservation with LoadingData
    Merged those two classes and added the database functionality.

---
+++ ECharger/ECharger/Models/Data_Models/Reservation.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECharger
+{
+    class Reservations
+    {
+        public int ID { get; set; }
+        public DateTime StartTime { get; set; }
+        private DateTime endTime { get; set; }
+        public double TotalPrice { get; private set; }
+        public int ChargingStationID { get; set; }
+        public ChargingStation ChargingStation { get; set; }
+        public int UserCardID { get; set; }
+        public UserCard UserCard { get; set; }
+
+        public Reservations(DateTime startTime, DateTime endTime, ChargingStation chargingStation, UserCard userCard)
+        {
+            ChargingStation = chargingStation;
+            StartTime = startTime;
+            EndTime = endTime;
+            UserCard = userCard;
+        }
+
+        public DateTime EndTime {
+            get { return endTime; }
+            set
+            {
+                TimeSpan timeSpan = EndTime - StartTime;
+                int totalMinutes = (int) timeSpan.TotalMinutes;
+
+                if (totalMinutes < 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value),
+                        value, $"{nameof(EndTime)} has to be bigger than {nameof(StartTime)}");
+                }
+
+                endTime = value;
+
+                TotalPrice = ChargingStation.PricePerMinute * totalMinutes;
+            }
+        }
+
+        public string toString()
+        {
+            return $"Start Time: {StartTime}\nEnd Time: {EndTime}\nTotal Price: {TotalPrice}\nCharging Station Id: {ChargingStationID}\nUser Card Id: {UserCardID}";
+        }
+    }
+}
