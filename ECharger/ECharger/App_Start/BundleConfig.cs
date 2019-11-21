565ea23a9bf587da2cc6ed7f6619615b82e99c0b
Ricardo <33632491+rmcsilva@users.noreply.github.com>
2019-11-21 17:35:38 +0000

    Added: JQuery InputMask To Project
    InputMask will be used when the user needs to insert a reservation date

--- ECharger/ECharger/App_Start/BundleConfig.cs
+++ ECharger/ECharger/App_Start/BundleConfig.cs
@@ -14,6 +14,13 @@ namespace ECharger
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/inputmask").Include(
+            "~/Scripts/inputmask/inputmask.js",
+            "~/Scripts/inputmask/jquery.inputmask.js",
+            "~/Scripts/inputmask/inputmask.extensions.js",
+            "~/Scripts/inputmask/inputmask.date.extensions.js",
+            "~/Scripts/inputmask/inputmask.numeric.extensions.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
