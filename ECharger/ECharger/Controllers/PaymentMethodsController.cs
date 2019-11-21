28e9a9e88ef21c25314fddd102d7849061bc2c45
Ricardo <33632491+rmcsilva@users.noreply.github.com>
2019-11-21 01:26:17 +0000

    Added: PaymentMethodsController
    Controller to handle the payment methods the client will use

---
+++ ECharger/ECharger/Controllers/PaymentMethodsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ECharger;
+using ECharger.Models;
+
+namespace ECharger.Controllers
+{
+    public class PaymentMethodsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: PaymentMethods
+        public ActionResult Index()
+        {
+            var paymentMethods = db.PaymentMethods.Include(p => p.UserCard);
+            return View(paymentMethods.ToList());
+        }
+
+        // GET: PaymentMethods/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PaymentMethod paymentMethod = db.PaymentMethods.Find(id);
+            if (paymentMethod == null)
+            {
+                return HttpNotFound();
+            }
+            return View(paymentMethod);
+        }
+
+        // GET: PaymentMethods/Create
+        public ActionResult Create()
+        {
+            ViewBag.UserCardID = new SelectList(db.UserCards, "ID", "Email");
+            return View();
+        }
+
+        // POST: PaymentMethods/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,UserCardID,Value")] PaymentMethod paymentMethod)
+        {
+            if (ModelState.IsValid)
+            {
+                db.PaymentMethods.Add(paymentMethod);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.UserCardID = new SelectList(db.UserCards, "ID", "Email", paymentMethod.UserCardID);
+            return View(paymentMethod);
+        }
+
+        // GET: PaymentMethods/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PaymentMethod paymentMethod = db.PaymentMethods.Find(id);
+            if (paymentMethod == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UserCardID = new SelectList(db.UserCards, "ID", "Email", paymentMethod.UserCardID);
+            return View(paymentMethod);
+        }
+
+        // POST: PaymentMethods/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,UserCardID,Value")] PaymentMethod paymentMethod)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(paymentMethod).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UserCardID = new SelectList(db.UserCards, "ID", "Email", paymentMethod.UserCardID);
+            return View(paymentMethod);
+        }
+
+        // GET: PaymentMethods/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PaymentMethod paymentMethod = db.PaymentMethods.Find(id);
+            if (paymentMethod == null)
+            {
+                return HttpNotFound();
+            }
+            return View(paymentMethod);
+        }
+
+        // POST: PaymentMethods/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            PaymentMethod paymentMethod = db.PaymentMethods.Find(id);
+            db.PaymentMethods.Remove(paymentMethod);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
