6081683d4ce59d791ffee07f4f1ad7874b70c959
Ricardo <33632491+rmcsilva@users.noreply.github.com>
2019-11-26 21:53:51 +0000

    Added: Web API Support
    Project now supports the use of web APIs

--- ECharger/ECharger/Global.asax.cs
+++ ECharger/ECharger/Global.asax.cs
@@ -5,6 +5,9 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Http;
+using AutoMapper;
+using ECharger.App_Start;
 
 namespace ECharger
 {
@@ -12,6 +15,7 @@ namespace ECharger
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
