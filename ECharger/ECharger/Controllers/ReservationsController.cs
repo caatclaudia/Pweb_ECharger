b4f924a9511569f15709643566d0aa864c566012
caatclaudia <a21270617@isec.pt>
2019-12-04 20:06:00 +0000

    Added: Details View for Reservations

--- ECharger/ECharger/Controllers/ReservationsController.cs
+++ ECharger/ECharger/Controllers/ReservationsController.cs
@@ -56,6 +56,10 @@ namespace ECharger.Controllers
             {
                 return View("CompanyDetails", reservation);
             }
+            if (User.IsInRole(RoleName.User))
+            {
+                return View("UserDetails", reservation);
+            }
 
             return View(reservation);
         }
